86445151ef6c38a2905c619c33bf277316d29ef2
Nicholas Melnick <nick@abstractwankery.com>
2014-10-20 13:59:42 +0000

    TRIAL

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ for module_name in whatbot whatbot-*/ Task-whatbot ; do
 	module_name=${module_name%/}
 	MOD=`grep all_from $module_name/Makefile.PL | sed 's/all_from .\(.*pm\)../\1/'`
 	VER=`grep 'VERSION =' $module_name/$MOD | sed 's/^.*VERSION = .\([0-9]\+.[0-9]\+\).*/\1/'`
-	PACK="$module_name-$VER.tar.gz"
+	PACK="$module_name-$VER-TRIAL.tar.gz"
 	echo $PACK
 	tar cf - $module_name | gzip > ../$PACK
 done
