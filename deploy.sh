963b2850760359f22d596164f67ceae14d82c40e
Nicholas Melnick <nick@abstractwankery.com>
2014-10-20 15:19:13 +0000

    whatbot to Whatbot

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ mkdir staging
 cd staging
 git clone git@github.com:nmelnick/whatbot.git
 cd whatbot
-for module_name in whatbot whatbot-*/ Task-whatbot ; do
+for module_name in Whatbot Whatbot-*/ Task-Whatbot ; do
 	module_name=${module_name%/}
 	MOD=`grep all_from $module_name/Makefile.PL | sed 's/all_from .\(.*pm\)../\1/'`
 	VER=`grep 'VERSION =' $module_name/$MOD | sed 's/^.*VERSION = .\([0-9]\+.[0-9]\+\).*/\1/'`
