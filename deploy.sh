032ff937bd5e52a5466e17de53f3abd82f3720c0
Nicholas Melnick <nick@abstractwankery.com>
2014-10-22 16:59:11 -0500

    leading zero

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ for module_name in Whatbot Whatbot-*/ Task-Whatbot ; do
 	module_name=${module_name%/}
 	MOD=`grep all_from $module_name/Makefile.PL | sed 's/all_from .\(.*pm\)../\1/'`
 	VER=`grep 'VERSION =' $module_name/$MOD | sed 's/^.*VERSION = .\([0-9]\+.[0-9]\+\).*/\1/'`
-	SUFFIX="_$1"
+	SUFFIX="$(printf '_%02d' $1)"
 	VER=$VER$SUFFIX
 	PACK="$module_name-$VER.tar.gz"
 	echo $PACK
